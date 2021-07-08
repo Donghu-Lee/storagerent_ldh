cd889f9e98f4ce68cdae638f18307dea50a56e8c
Donghu-Lee <84304082+Donghu-Lee@users.noreply.github.com>
2021-07-08 19:11:48 +0900

    Update PaymentController.java

--- payment/src/main/java/storagerent/PaymentController.java
+++ payment/src/main/java/storagerent/PaymentController.java
@@ -9,7 +9,31 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.util.List;
 
- @RestController
- public class PaymentController {
+@RestController
+public class PaymentController {
 
- }
+    @GetMapping("/callmemleak")
+    public void callMemLeak() {
+    try {
+        this.memLeak();
+    } catch (Exception e) {
+        e.printStackTrace();
+    }
+    }
+
+    public void memLeak() throws NoSuchFieldException, ClassNotFoundException, IllegalAccessException {
+        Class unsafeClass = Class.forName("sun.misc.Unsafe");
+        
+        Field f = unsafeClass.getDeclaredField("theUnsafe");
+        f.setAccessible(true);
+        Unsafe unsafe = (Unsafe) f.get(null);
+        System.out.print("4..3..2..1...");
+        try {
+            for(;;)
+            unsafe.allocateMemory(1024*1024);
+        } catch(Error e) {
+            System.out.println("Yoo---Ho!");
+            e.printStackTrace();
+        }
+    }
+}
